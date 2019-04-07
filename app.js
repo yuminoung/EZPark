cec15e12d7e8a36a474de7b4bb06dab6adcc0c9e
yuminoung <yuminoung.dev@gmail.com>
2019-04-07 18:01:28 +1000

    added html files

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const app = express()
+const fetch = require('node-fetch')
+
+// const controller = require(controller)
+
+const apiUrl = 'https://data.melbourne.vic.gov.au/resource/dtpv-d4pf.json'
 
 
-app.get('/', (req, res) => res.send('Hello'))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/src/index.html')
+    // fetch(apiUrl).then(res => res.json()).then(json => res.send(json)).catch(err => console.log(err))
+})
+
+// app.get('/hello', controller.getHello)
+
+
+// app.get('/result', (req, res) => {
+//     res.sendFile(__dirname + '/src/result.html')
+
+// })
+
+// app.use('/')
 
 PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => 'Listening...')
+
+//https://data.melbourne.vic.gov.au/resource/dtpv-d4pf.json
