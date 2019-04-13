9a0a05ed16d45ed3a8b1e8e7f2b5c9efc82076d1
yuminoung <yuminoung.dev@gmail.com>
2019-04-13 16:20:31 +1000

    some visual update on index.html page

--- routes/routes.js
+++ routes/routes.js
@@ -4,11 +4,6 @@ var router = express.Router()
 var carpark = require('../controllers/carpark.js')
 
 
-// //show all available street parking in melbourne CBD
-// router.get('/carpark', controller.findAllParkings)
-
-// //get one parking location by bay id
-// router.get('/carpark/:id', controller.findOneParking)
 
 router.get('/carpark', carpark.index)
 router.get('/carpark/:id', carpark.show)
