3adb9e61356a5bdbe9cd3c54acbe07d1410b67dc
Ziyi Li <ziyil6@student.unimelb.edu.au>
2019-05-12 01:23:04 +1000

    add userprofile page

--- routes/routes.js
+++ routes/routes.js
@@ -8,11 +8,13 @@ var signup = require('../controllers/signup.js')
 var login = require('../controllers/login.js')
 var error = require('../controllers/error.js')
 var favourites = require("../controllers/favourites.js")
+var userprofile = require("../controllers/userprofile.js")
 
 //home
 router.get('/', home.index);
 router.get('/signup', signup.index);
 router.get('/login', login.index);
+router.get('/userprofile', userprofile.index);
 
 
 //search
