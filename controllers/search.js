e59f5eaa7d72b053c5b06d598281659d53edb0a9
yuminoung <yuminoung.dev@gmail.com>
2019-06-02 15:02:45 +1000

    bug fixes

--- controllers/search.js
+++ controllers/search.js
@@ -148,7 +148,7 @@ var store = function (req, res) {
                     }
                 })
             if (req.user) {
-                res.send(req.user);
+                res.send(req.user._id);
             }
             res.redirect('/search/' + req.body.search + '?filter=' + req.body.filter)
 
